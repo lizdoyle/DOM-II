e83c46d11f786b6159eb40f19722b8968bcdc31d
lizdoyle <lizdoyle1121@gmail.com>
2019-07-12 22:42:40 -0400

    starting on stretch :(

---
+++ stretch-assignment/index.js
@@ -0,0 +1,33 @@
+const blocks = document.querySelector('.blocks');
+const redBlock = document.querySelector('.block--red');
+const blueBlock = document.querySelector('.block--blue');
+const greenBlock = document.querySelector('.block--green');
+const pinkBlock = document.querySelector('.block--pink');
+const grayBlock = document.querySelector('.block--gray');
+
+
+// Rockets:
+//   * When a block is clicked, it should go to the top of the stack.
+
+const rockets = document.querySelectorAll(".block");
+
+rockets.addEventListener('click', () => {
+
+});
+
+
+
+
+// Travelers:
+// * While a mouse is clicked down on a box, it should move to the right 
+// (It should coninuously move, there is no limit on how far it will go).
+
+const travelers = document.querySelectorAll('.block');
+
+// let travel = setInterval()
+
+travelers.addEventListener('click', (move) => {
+    var pos = 0;
+    var id = setInterval (frame, )
+
+});
